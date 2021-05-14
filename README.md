ecc3e216ba9a85d3b3115d6a3f697042d40ff425
SC <chaolic6505@gmail.com>
2021-05-14 05:08:53 -0700

    create autobind decorator

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Drag&Drop
+#Drag&Drop
+
+## 🚀 Start The Program <a name = "deployment"></a>
+
+To start the program you will need to go to the program directory and open up your terminal and
+
+1. Type `npm install` to download all the dependencies.
+2. Type `npm start` to start the app.
+
+## ⛏️ Web Stacks <a name = "built_using"></a>
+
+- Typescript
+- HTML
+- CSS
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,70 @@
+//autobind decorator
+const autobind = (
+	target: any,
+	methodName: string,
+	descriptor: PropertyDescriptor,
+) => {
+	const originalMethod = descriptor.value;
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+
+	return adjDescriptor;
+};
+
+//  Class ProjectInput
 class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
-  constructor() {
-    //data fetched here won't be null and will be type 'HTMLTemplateElement'
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+	constructor() {
+		//data fetched here won't be null and will be type 'HTMLTemplateElement'
+		this.templateElement = document.getElementById(
+			'project-input',
+		)! as HTMLTemplateElement;
 
-    //data fetched here won't be null and will be type 'HTMLDivElement'
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+		//data fetched here won't be null and will be type 'HTMLDivElement'
+		this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
-    // creates a copy of a node
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.attach();
-    
-  }
+		// creates a copy of a node
+		const importedNode = document.importNode(this.templateElement.content, true);
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = 'user-input';
 
-  private attach() {
-    // inserts a given element node at a given position relative to the element it is invoked upon.
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
-  }
+		this.titleInputElement = this.element.querySelector(
+			'#title',
+		) as HTMLInputElement;
+		this.descriptionInputElement = this.element.querySelector(
+			'#description',
+		) as HTMLInputElement;
+		this.peopleInputElement = this.element.querySelector(
+			'#people',
+		) as HTMLInputElement;
+
+		this.configure();
+		this.attach();
+	}
+	@autobind
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInputElement.value);
+	}
+
+	private configure() {
+		this.element.addEventListener('submit', this.submitHandler);
+	}
+	private attach() {
+		// inserts a given element node at a given position relative to the element it is invoked upon.
+		this.hostElement.insertAdjacentElement('afterbegin', this.element);
+	}
 }
 
 const prjInput = new ProjectInput();
